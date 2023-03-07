8016331e735beb8197ee7f6c02a29867d182dcf8
ysbc1247 <ystc1247@khu.ac.kr>
2023-03-07 17:15:02 +0900

    Refactoring Notice

--- src/main/java/com/keyworld/projectboard/controller/NoticeController.java
+++ src/main/java/com/keyworld/projectboard/controller/NoticeController.java
@@ -2,14 +2,12 @@ package com.keyworld.projectboard.controller;
 
 
 import com.keyworld.projectboard.domain.Notice;
-import com.keyworld.projectboard.dto.NoticeDto;
+import com.keyworld.projectboard.dto.NoticeDTO;
 import com.keyworld.projectboard.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,7 +16,7 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     @GetMapping("/notice")
-    public NoticeDto.Response getNotice(){
+    public NoticeDTO.Response getNotice(){
         return noticeService.searchById(noticeService.getNoticeRepository().count());
     }
     @PostMapping("/admin/notice/update")
