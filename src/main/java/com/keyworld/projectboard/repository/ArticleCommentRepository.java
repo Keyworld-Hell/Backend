59bc380ec113e26dd45697f12e6314506e94bb5f
ysbc1247 <ystc1247@khu.ac.kr>
2023-03-09 16:45:02 +0900

    Refactoring Article and Comments

--- src/main/java/com/keyworld/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/keyworld/projectboard/repository/ArticleCommentRepository.java
@@ -1,7 +1,6 @@
 package com.keyworld.projectboard.repository;
 
 import com.keyworld.projectboard.domain.ArticleComment;
-import com.keyworld.projectboard.domain.QArticleComment;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,21 +13,10 @@ import java.util.List;
 
 @RepositoryRestResource
 public interface ArticleCommentRepository extends
-        JpaRepository<ArticleComment, Long>,
-        QuerydslPredicateExecutor<ArticleComment>,
-        QuerydslBinderCustomizer<QArticleComment> {
+        JpaRepository<ArticleComment, Long>{
 
     List<ArticleComment> findByArticle_Id(Long articleId);
     void deleteById(Long articleCommentId);
 
-    @Override
-    default void customize(QuerydslBindings bindings, QArticleComment root) {
-        bindings.excludeUnlistedProperties(true);
-        bindings.including(root.author, root.content, root.createdAt);
-
-        bindings.bind(root.author).first(StringExpression::containsIgnoreCase);
-        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
-        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
-    }
 
 }
