385e8da694e7fd0dbb451b76e48bab76b64cb874
ystc1247 <ystc1247@khu.ac.kr>
2023-03-09 10:27:00 +0900

    Deleted QEntity

--- src/main/java/com/keyworld/projectboard/repository/InquiryRepository.java
+++ src/main/java/com/keyworld/projectboard/repository/InquiryRepository.java
@@ -1,7 +1,6 @@
 package com.keyworld.projectboard.repository;
 
 import com.keyworld.projectboard.domain.Inquiry;
-import com.keyworld.projectboard.domain.QInquiry;
 import com.keyworld.projectboard.repository.querydsl.InquiryRepositoryCustom;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
@@ -16,23 +15,11 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 
 public interface InquiryRepository extends
-        JpaRepository<Inquiry, Long>,
-        InquiryRepositoryCustom,
-        QuerydslPredicateExecutor<Inquiry>,
-        QuerydslBinderCustomizer<QInquiry> {
+        JpaRepository<Inquiry, Long>{
 
     Page<Inquiry> findByTitleContaining(String title, Pageable pageable);
 
     Page<Inquiry> findByContentContaining(String content, Pageable pageable);
 
     void deleteById(Long inquiryId);
-
-    default void customize(QuerydslBindings bindings, QInquiry root) {
-        bindings.excludeUnlistedProperties(true);
-        bindings.including(root.title, root.content, root.createdAt, root.createdBy);
-        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
-        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
-        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
-        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
-    }
 }
