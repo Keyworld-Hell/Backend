0e42cf32ef760bac8dfb1e0d57102c1840acfc3b
ystc1247 <ystc1247@khu.ac.kr>
2023-03-09 10:26:11 +0900

    Refactoring Controller

--- src/main/java/com/keyworld/projectboard/controller/NoticeController.java
+++ src/main/java/com/keyworld/projectboard/controller/NoticeController.java
@@ -2,31 +2,54 @@ package com.keyworld.projectboard.controller;
 
 
 import com.keyworld.projectboard.domain.Notice;
-import com.keyworld.projectboard.dto.NoticeDto;
+import com.keyworld.projectboard.dto.NoticeDTO;
 import com.keyworld.projectboard.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/")
 public class NoticeController {
+
     private final NoticeService noticeService;
 
-    @GetMapping("/notice")
-    public NoticeDto.Response getNotice(){
-        return noticeService.searchById(noticeService.getNoticeRepository().count());
+    public NoticeController(NoticeService noticeService) {
+        this.noticeService = noticeService;
     }
-    @PostMapping("/admin/notice/update")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void saveNotice(MultipartHttpServletRequest multiRequest) throws Exception {
-        Notice notice = new Notice();
-        notice.setTitle(multiRequest.getParameter("title"));
-        notice.setContent(multiRequest.getParameter("content"));
-        noticeService.save(notice);
+
+    @PostMapping("/adm/notice/new")
+    public ResponseEntity<Void> createNotice(@ModelAttribute NoticeDTO noticeDto) throws IOException {
+        noticeService.createNotice(noticeDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}/notice")
+    public ResponseEntity<NoticeDTO> getNotice(@PathVariable Long id) throws IOException {
+        NoticeDTO noticeDto = noticeService.getNotice(id);
+        return ResponseEntity.ok(noticeDto);
+    }
+
+    @GetMapping("/adm/notice")
+    public List<Notice> getNoticeList() throws IOException{
+        return noticeService.getNoticeList();
+    }
+
+    @PutMapping("/adm/notice/update/{id}")
+    public ResponseEntity<Void> updateNotice(@PathVariable Long id, @ModelAttribute NoticeDTO noticeDto) throws IOException {
+        noticeService.updateNotice(id, noticeDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/adm/notice/delete/{id}")
+    public ResponseEntity<Void> deleteNotice(@PathVariable Long id) {
+        noticeService.deleteNotice(id);
+        return ResponseEntity.ok().build();
     }
 }
