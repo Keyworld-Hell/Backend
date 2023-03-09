00c95969b17a14479405957869eb8641bbbf31cc
ysbc1247 <ystc1247@khu.ac.kr>
2023-03-09 16:45:10 +0900

    Refactoring Article and Comments

--- src/main/java/com/keyworld/projectboard/dto/request/ArticleRequest.java
+++ src/main/java/com/keyworld/projectboard/dto/request/ArticleRequest.java
@@ -5,18 +5,21 @@ import com.keyworld.projectboard.dto.ArticleDto;
 public record ArticleRequest(
         String author,
         String title,
-        String content
+        String content,
+
+        String password
 ) {
 
-    public static ArticleRequest of(String author, String title, String content) {
-        return new ArticleRequest(author, title, content);
+    public static ArticleRequest of(String author, String title, String content, String password) {
+        return new ArticleRequest(author, title, content, password);
     }
 
     public ArticleDto toDto() {
         return ArticleDto.of(
                 author,
                 title,
-                content
+                content,
+                password
         );
     }
 }
