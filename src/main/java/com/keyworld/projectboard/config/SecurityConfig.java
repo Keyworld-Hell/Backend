cedfa973e81544259056383610e4e1f57a1c3e84
ystc1247 <ystc1247@khu.ac.kr>
2023-03-20 09:57:21 +0900

    Deactivated Security for test

--- src/main/java/com/keyworld/projectboard/config/SecurityConfig.java
+++ src/main/java/com/keyworld/projectboard/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+/*
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -52,4 +53,4 @@ public class SecurityConfig {
     }
 }
 
-
+*/
