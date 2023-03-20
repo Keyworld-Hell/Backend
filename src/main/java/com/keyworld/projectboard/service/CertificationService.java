f726ad38356080a8efeae6fff20d74ad5eb8fa51
ystc1247 <ystc1247@khu.ac.kr>
2023-03-20 09:58:06 +0900

    Refactoring Certifiaction for files

--- src/main/java/com/keyworld/projectboard/service/CertificationService.java
+++ src/main/java/com/keyworld/projectboard/service/CertificationService.java
@@ -2,11 +2,16 @@ package com.keyworld.projectboard.service;
 
 import com.keyworld.projectboard.domain.Certification;
 import com.keyworld.projectboard.domain.Company;
+import com.keyworld.projectboard.domain.Notice;
 import com.keyworld.projectboard.dto.CertificationDTO;
+import com.keyworld.projectboard.dto.NoticeDTO;
 import com.keyworld.projectboard.repository.CertificationRepository;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -15,9 +20,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Getter
 public class CertificationService {
+
+    private final FileService fileService;
     @Autowired
     private CertificationRepository repository;
+
+    public CertificationService(FileService fileService) {
+        this.fileService = fileService;
+    }
+
 
     public List<Certification> getAll() {
         return repository.findAll();
@@ -26,32 +39,56 @@ public class CertificationService {
     public List<Certification> findByLanguage(Boolean language){
         return repository.findByLanguage(language);
     }
-    public byte[] getByIdFile(Long id) {
-        Certification entity = getById(id);
-        return entity.getFile();
+
+    public CertificationDTO getById(Long id) throws IOException {
+        // Retrieve the certification from the database
+        Certification certification = repository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException());
+
+        CertificationDTO certificationDTO = new CertificationDTO();
+        certificationDTO.setTitle(certification.getTitle());
+        certificationDTO.setLanguage(certification.getLanguage());
+
+        if (certification.getFilePath() != null) {
+            Resource fileResource = fileService.downloadFile(certification.getFilePath());
+            certificationDTO.setFile((MultipartFile) fileResource.getFile());
+        }
+
+        return certificationDTO;
     }
 
-    public Certification getById(Long id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException());
+    public void save(CertificationDTO dto) throws IOException {
+        Certification certification = new Certification();
+        certification.setTitle(dto.getTitle());
+        certification.setLanguage(dto.getLanguage());
+        if (dto.getFile() != null) {
+            String fileName = dto.getFile().getOriginalFilename();
+            String filePath = "/certifications/" + fileName;
+            certification.setFilePath(filePath);
+            fileService.uploadFile(dto.getFile(), filePath);
+        }
     }
 
-    public Certification save(CertificationDTO dto) throws IOException {
-        String title = dto.getTitle();
-        Certification entity = new Certification(title, dto.getFile().getBytes());
-        return repository.save(entity);
-    }
+    public void update(Long id, CertificationDTO certificationDTO) throws IOException {
+        Certification certification = repository.findById(id).orElse(null);
 
-    public Certification update(Long id, CertificationDTO dto) throws IOException {
-        Certification entity = getById(id);
-        String title = dto.getTitle();
-        entity.setTitle(title);
-        entity.setFile(dto.getFile().getBytes());
-        return repository.save(entity);
-    }
+        if (certification == null) {
+            return;
+        }
 
+        certification.setTitle(certificationDTO.getTitle());
+        certification.setLanguage(certificationDTO.getLanguage());
+
+        if (certificationDTO.getFile() != null) {
+            String fileName = certificationDTO.getFile().getOriginalFilename();
+            String filePath = "/certifications/" + fileName;
+            certification.setFilePath(filePath);
+            fileService.uploadFile(certificationDTO.getFile(), filePath);
+        }
+
+        repository.save(certification);
+    }
     public void delete(Long id) {
-        Certification entity = getById(id);
-        repository.delete(entity);
+        repository.deleteById(id);
     }
 }
