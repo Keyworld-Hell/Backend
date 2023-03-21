b160311781df46599130aef6f3921f9577717f40
ystc1247 <ystc1247@khu.ac.kr>
2023-03-21 22:27:10 +0900

    Disabled Security

--- src/main/java/com/keyworld/projectboard/config/SecurityConfig.java
+++ src/main/java/com/keyworld/projectboard/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 package com.keyworld.projectboard.config;
-
+/*
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
