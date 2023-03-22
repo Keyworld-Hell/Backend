94a6d2f2629ac20f4b3f41c13af5e11a0ab52839
ystc1247 <ystc1247@khu.ac.kr>
2023-03-22 15:55:23 +0900

    Refactored data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-insert into NOTICE (title, content) values ('Sloth, two-toed tree', 'Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat.');
+INSERT INTO ARTICLE (adm,author,content,password,title) values (1, 'asdf','asdf','asdf', 'asdf');
