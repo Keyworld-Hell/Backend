f8715c714ec721868dded5a6aa6a35d4a22b9027
ysbc1247 <ystc1247@khu.ac.kr>
2023-04-10 17:04:23 +0900

    To-do edit gitignore to serve Qentities

---
+++ src/main/generated/com/keyworld/projectboard/domain/QBaseTime 3.java
@@ -0,0 +1,43 @@
+package com.keyworld.projectboard.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBaseTime is a Querydsl query type for BaseTime
+ */
+@Generated("com.querydsl.codegen.DefaultSupertypeSerializer")
+public class QBaseTime extends EntityPathBase<BaseTime> {
+
+    private static final long serialVersionUID = 408953749L;
+
+    public static final QBaseTime baseTime = new QBaseTime("baseTime");
+
+    public final DateTimePath<java.time.LocalDateTime> CreatedDateTime = createDateTime("CreatedDateTime", java.time.LocalDateTime.class);
+
+    public final DateTimePath<java.time.LocalDateTime> createdDateTime = createDateTime("createdDateTime", java.time.LocalDateTime.class);
+
+    public final DateTimePath<java.time.LocalDateTime> ModifiedDateTime = createDateTime("ModifiedDateTime", java.time.LocalDateTime.class);
+
+    public final DateTimePath<java.time.LocalDateTime> modifiedDateTime = createDateTime("modifiedDateTime", java.time.LocalDateTime.class);
+
+    public QBaseTime(String variable) {
+        super(BaseTime.class, forVariable(variable));
+    }
+
+    public QBaseTime(Path<? extends BaseTime> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBaseTime(PathMetadata metadata) {
+        super(BaseTime.class, metadata);
+    }
+
+}
+
---
+++ src/main/generated/com/keyworld/projectboard/domain/QLocks 2.java
@@ -0,0 +1,68 @@
+package com.keyworld.projectboard.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QLocks is a Querydsl query type for Locks
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QLocks extends EntityPathBase<Locks> {
+
+    private static final long serialVersionUID = 1304441265L;
+
+    public static final QLocks locks = new QLocks("locks");
+
+    public final QAuditingFields _super = new QAuditingFields(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final StringPath description = createString("description");
+
+    public final StringPath feature = createString("feature");
+
+    public final ListPath<String, StringPath> filePathList = this.<String, StringPath>createList("filePathList", String.class, StringPath.class, PathInits.DIRECT2);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath language = createBoolean("language");
+
+    public final StringPath material = createString("material");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public final NumberPath<Long> number = createNumber("number", Long.class);
+
+    public final StringPath path1 = createString("path1");
+
+    public final StringPath path2 = createString("path2");
+
+    public final StringPath purpose = createString("purpose");
+
+    public final StringPath surface = createString("surface");
+
+    public final StringPath title = createString("title");
+
+    public QLocks(String variable) {
+        super(Locks.class, forVariable(variable));
+    }
+
+    public QLocks(Path<? extends Locks> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QLocks(PathMetadata metadata) {
+        super(Locks.class, metadata);
+    }
+
+}
+
